87bab75575c088cddd311dcf33e9bce27004f19c
sreekeshv31 <61742339+sreekeshv31@users.noreply.github.com>
2020-09-12 12:03:37 +0530

    Add files via upload

---
+++ two-sum.cpp
@@ -0,0 +1,30 @@
+/* Two sum 
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1]. */
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> hash;
+        for(int i=0;i<nums.size();i++)
+        {
+            int numToFind = target - nums[i];
+            if(hash.find(numToFind)!=hash.end())
+            {
+                return {hash[numToFind],i};
+            }
+            hash[nums[i]] = i;
+            
+        }
+        return {};
+        
+    }
+};
